9392236d895ecd2844d56472df1680c4819ae03d
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-09-24 03:27:14 +0300

    Send credentials with fetch requests

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -3,6 +3,7 @@ import { getToken } from "./token";
 export async function commonFetch(input: RequestInfo, init: RequestInit = {}) {
 	return fetch(input, {
 		...init,
+		credentials: "include",
 		headers: {
 			...(init.headers || {}),
 			authorization: getToken(),
